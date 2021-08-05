91362c32f87f83e36fcfdcf38ed04fd1771bcd8e
iket0731 <60433699+iket0731@users.noreply.github.com>
2021-08-05 09:27:50 +0900

    Implement single step go-back

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,22 @@
 import * as vscode from 'vscode';
 
+let pos: vscode.Position;
+let doc: vscode.TextDocument;
+
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('goto-definition-history.revealDefinition', () => {
-		revealDefinition();
-	});
+	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand('goto-definition-history.revealDefinition', (editor) => {
+			vscode.commands.executeCommand("editor.action.revealDefinition");
+			doc = editor.document
+			pos = editor.selection.active;
+		})
+	);
 
-	context.subscriptions.push(disposable);
-}
-
-export function deactivate() {
-}
-
-function revealDefinition() {
-	vscode.commands.executeCommand("editor.action.revealDefinition");
+	context.subscriptions.push(
+		vscode.commands.registerCommand('goto-definition-history.goBack', async () => {
+			const editor = await vscode.window.showTextDocument(doc);
+			editor.selection = new vscode.Selection(pos, pos);
+			editor.revealRange(new vscode.Range(pos, pos));
+		})
+	);
 }
